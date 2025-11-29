100af7c7871a0b6229450f87b8e61182d6afaee5
Benjamin1478557 <1478557@senati.pe>
2025-11-29 01:46:53 -0500

    correccion de adaptador

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,33 +1,36 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import react from '@astrojs/react';
+import react from "@astrojs/react";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
-import vercel from '@astrojs/vercel';
+import vercel from "@astrojs/vercel";
 
-import sitemap from '@astrojs/sitemap';
-
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site:'https://front-end-proyecto-wor-git-ceef90-benjaminpari1478557s-projects.vercel.app',
-  output: 'static', //server
+  site: "https://front-end-proyecto-wor-git-ceef90-benjaminpari1478557s-projects.vercel.app",
+  output: "static", //server
   integrations: [
-    react(), 
+    react(),
     sitemap({
-      filter: (page) => page !== 'https://front-end-proyecto-wor-git-ceef90-benjaminpari1478557s-projects.vercel.app/core-tacana-wits-7b345',
+      filter: (page) =>
+        page !==
+        "https://front-end-proyecto-wor-git-ceef90-benjaminpari1478557s-projects.vercel.app/core-tacana-wits-7b345",
     }),
   ],
 
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
   },
 
-  // adapter: vercel({
-  //   isr:{
+  adapter: vercel(
+  //   {
+  //   isr: {
   //     expiration: 60 * 60,
-  //   }
-  // })
+  //   },
+  // }
+),
 });
